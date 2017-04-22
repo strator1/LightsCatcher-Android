27e15f240f6e7827477260f3304f0f440cc5591b
johu <johannes.huber@hs-augsburg.de>
2017-04-22 15:41:32 +0200

    changed info-button

--- LightsCatcher/app/src/main/java/com/hs_augsburg_example/lightscatcher/SettingsActivity.java
+++ LightsCatcher/app/src/main/java/com/hs_augsburg_example/lightscatcher/SettingsActivity.java
@@ -1,31 +1,15 @@
 package com.hs_augsburg_example.lightscatcher;
 
 
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.hs_augsburg_example.lightscatcher.singletons.UserInformation;
 import com.hs_augsburg_example.lightscatcher.utils.ActivityRegistry;
-
-import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
