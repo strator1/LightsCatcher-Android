50a91955d888fa3910f70b34b1bf2368f3b7ed4c
Quirin Buechner <quirin.buechner@hs-augsburg.de>
2017-04-22 14:55:47 +0200

    added swipe-refresh to home activity

--- LightsCatcher/app/src/main/java/com/hs_augsburg_example/lightscatcher/HomeActivity.java
+++ LightsCatcher/app/src/main/java/com/hs_augsburg_example/lightscatcher/HomeActivity.java
@@ -3,6 +3,7 @@ package com.hs_augsburg_example.lightscatcher;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,19 +19,25 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.hs_augsburg_example.lightscatcher.camera.TakePictureActivity;
 import com.hs_augsburg_example.lightscatcher.dataModels.User;
+import com.hs_augsburg_example.lightscatcher.singletons.UserInformation;
 import com.hs_augsburg_example.lightscatcher.utils.ReverseFirebaseListAdapter;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private FirebaseListAdapter<User> adapter = null;
     private DatabaseReference usersDatabase = null;
     private Query top10 = null;
+    private TextView txtUserName;
+    private TextView txtUserRank;
+    private TextView txtUserScore;
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -41,8 +48,17 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        this.txtUserName = (TextView) findViewById(R.id.home_txt_username);
+        this.txtUserRank = (TextView) findViewById(R.id.home_txt_rank);
+        this.txtUserScore = (TextView)findViewById(R.id.home_txt_score);
 
-        // Connect to the Firebase database and query top10 users
+        // display information about current user:
+        this.refreshUserData();
+
+        this.swipeLayout = (SwipeRefreshLayout) findViewById(R.id.home_refreshLayout);
+        swipeLayout.setOnRefreshListener(this);
+
+        // query ordered list of users from firebase
         usersDatabase = FirebaseDatabase.getInstance().getReference("users");
         top10 = usersDatabase.orderByChild("points");
 
@@ -59,13 +75,9 @@ public class HomeActivity extends AppCompatActivity {
             }
         };
 
-
-
         // Assign adapter to ListView
-        final ListView listView = (ListView) findViewById(R.id.view_userRanking);
+        final ListView listView = (ListView) findViewById(R.id.list_userRanking);
         listView.setAdapter(adapter);
-
-           
     }
 
     @Override
@@ -87,8 +99,30 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    private void refreshUserItems() {
+    private void refreshUserData(){
+        User usr = UserInformation.shared.current;
+        if(usr != null)
+        {
+            txtUserName.setText(UserInformation.shared.current.name);
+            txtUserRank.setText("-");
+            txtUserScore.setText(UserInformation.shared.current.points);
+        }
+        else
+        {
+            txtUserName.setText("-");
+            txtUserRank.setText("-");
+            txtUserScore.setText("-");
+        }
+    }
+    private void refreshListItems() {
+        //show loading animation during loading
+        swipeLayout.setRefreshing(true);
 
+        // notify the view
+        this.adapter.notifyDataSetChanged();
+
+        //stop loading animation during loading
+        swipeLayout.setRefreshing(false);
     }
 
     private void navigateToCamera() {
@@ -96,4 +130,11 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Called when swiping the ranking-list
+     */
+    @Override
+    public void onRefresh() {
+        refreshListItems();
+    }
 }
